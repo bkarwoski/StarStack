7adb5190419a0143150d59f1e67420590e2353e6
Blake Karwoski <31117797+bkarwoski@users.noreply.github.com>
2020-06-28 15:52:30 -0700

    fixed reverse transform problem
    Former-commit-id: c414948cbaa1e6d96905f9dde3733fe34845fb8e

--- stack.py
+++ stack.py
@@ -7,20 +7,41 @@ import os
 import glob
 import copy
 from sklearn.neighbors import KDTree
+import sys
 from multiprocessing import Pool
 from util import *
 
 imgs = load_images()
+imgs.reverse()
 #how many stars to look for in each frame
-num_stars = 40
+num_stars = 200
 
 showStarCoords(imgs[0], getStarCoords(imgs[0], num_stars=num_stars))
 points = []
-for img in imgs:
+transforms = []
+for idx, img in enumerate(imgs):
     points.append(getStarCoords(img, num_stars=num_stars))
-affineTransform = getRigidTransform(points[0], points[1], numIter=8, img=imgs[0])
-out_size = (2 * imgs[-1].shape[1], 2 * imgs[-1].shape[0])
-im2_warp = cv2.warpAffine(imgs[-1], affineTransform[:2], out_size)
-im2_warp[:imgs[0].shape[0], :imgs[0].shape[1]] = imgs[0]
-im2_warp = cv2.cvtColor(im2_warp, cv2.COLOR_BGR2RGB)
-cv2.imwrite("affine_transformed_im2.jpg", im2_warp)
+    transforms.append(icpTransform(points[0], points[idx], threshold=20))
+# print(transforms)
+# affineTransform = icpTransform(points[0], points[-1])
+# affineTransform = getRigidTransform(points[0], points[1], numIter=8, img=imgs[0])
+frame_dim = [imgs[0].shape[1], imgs[0].shape[0]]
+img_stack_dim = (len(imgs), imgs[0].shape[0], imgs[0].shape[1],
+                 imgs[0].shape[2])
+img_stack = np.zeros((img_stack_dim), dtype=int)
+
+for idx, img in enumerate(imgs):
+    # warp_identity = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+    # img_warp = cv2.warpAffine(img, warp_identity, tuple(frame_dim), borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 0, 0))
+    img_warp = cv2.warpAffine(img, transforms[idx][:2], tuple(frame_dim), borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 0, 0), flags=cv2.WARP_INVERSE_MAP)
+    img_stack[idx] = img_warp
+print("images stacked")
+print(img_stack.shape)
+print("image stack size: ", str(sys.getsizeof(img_stack) / 1000000), " MB")
+
+max_img = np.ndarray.max(img_stack, axis=0)
+print("max img shape: ", str(max_img.shape))
+
+# cv2.imwrite("last_img_in_stack_warped.jpg", img_stack[-1])
+# cv2.cvtColor(max_img, cv2.COLOR_BGR2RGB)
+cv2.imwrite("stack_max.jpg", max_img)
