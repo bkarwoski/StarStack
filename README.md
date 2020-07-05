63abdef0f499a6cdafad045a18dc321e3691a83d
Blake Karwoski <31117797+bkarwoski@users.noreply.github.com>
2020-07-05 12:22:12 -0700

    using jpgs to align RAWs
    Former-commit-id: ab7a85a7e5cdb2e2225fd21d09476b9f855b2a91

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # StarStack
-stacking multiple star photographs for noise reduction
+stacking multiple star photographs for noise reduction.
+
+# Usage Directions
+Place desired images to be stacked in a folder in the same directory as stack.py, labeled "jpg". Run stack.py. The image registration will take roughly 30 seconds per image.
 
 
 ## TODO
--- stack.py
+++ stack.py
@@ -10,8 +10,10 @@ from sklearn.neighbors import KDTree
 import sys
 from util import *
 
-imgs_path = os.path.join(pathlib.Path().absolute(), "jpg/*")
+imgs_path = os.path.join(pathlib.Path().absolute(), "images/*")
+raw_path = os.path.join(pathlib.Path().absolute(), "raw/*")
 imgs_list = sorted(glob.glob(imgs_path))
+raw_list = sorted(glob.glob(raw_path))
 #how many stars to look for in each frame
 num_stars = 100
 
@@ -22,8 +24,11 @@ init_img = load_image(imgs_list[0])
 points_init = get_star_coords(init_img, num_stars=num_stars)
 img_stack = np.zeros((init_img.shape), dtype=np.float32)
 
-for fname in imgs_list[1:]:
+denoise_idxs = [x ** 2 - 1 for x in range(1, 12)]
+
+for idx, fname in enumerate(imgs_list[1:]):
     img = load_image(fname)
+    raw = load_image(raw_list[idx])
     # points = getStarCoords(img, num_stars=num_stars)
     transform = ecc_transform(init_img, img, prior=trans_prior)
     # print(transform)
@@ -31,12 +36,20 @@ for fname in imgs_list[1:]:
     trans_prior = transform
     img_stack_dim = img.shape
     frame_dim = (init_img.shape[1], init_img.shape[0])
-    img_warp = cv2.warpPerspective(img, transform, frame_dim,
+    img_warp = cv2.warpPerspective(raw, transform, frame_dim,
     borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 255, 0),
     flags=cv2.WARP_INVERSE_MAP)
     img_stack += img_warp
-    # img_stack = np.maximum.reduce([img_stack, img_warp])
-    print(fname, "stacked")
+    if idx in denoise_idxs:
+        center = (img_stack.shape[0] // 2, img_stack.shape[1] // 2)
+        h = 200
+        w = 300
+        img_out = img_stack[center[0] - h:center[0] + h,
+                            center[1] - w:center[1] + w] / (idx + 1)
+        out = str(idx + 1) + '_frames_mean.jpg'
+        print(out) 
+        cv2.imwrite(out, img_out)
+    print(str(idx), fname, "stacked")
 img_stack /= len(imgs_list)
 out_name = "stack_mean.jpg"
 cv2.imwrite(out_name, img_stack)
