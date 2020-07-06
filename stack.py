3403a7a9aaa6332216e59412013d7322bace823a
Blake Karwoski <31117797+bkarwoski@users.noreply.github.com>
2020-07-05 17:26:59 -0700

    fixed bug where first frame ignored. Improved print statements, removed transforms debugging tool

--- stack.py
+++ stack.py
@@ -10,34 +10,26 @@ from sklearn.neighbors import KDTree
 import sys
 from util import *
 
-imgs_path = os.path.join(pathlib.Path().absolute(), "images/*")
+jpgs_path = os.path.join(pathlib.Path().absolute(), "jpg/*")
 raw_path = os.path.join(pathlib.Path().absolute(), "raw/*")
-imgs_list = sorted(glob.glob(imgs_path))
+jpgs_list = sorted(glob.glob(jpgs_path))
 raw_list = sorted(glob.glob(raw_path))
-#how many stars to look for in each frame
-num_stars = 100
 
-transforms = []
 trans_prior = np.eye(3, 3, dtype=np.float32)
-transforms.append(trans_prior)
-init_img = load_image(imgs_list[0])
-points_init = get_star_coords(init_img, num_stars=num_stars)
-img_stack = np.zeros((init_img.shape), dtype=np.float32)
+init_img = load_image(jpgs_list[0])
+img_stack = init_img.astype(np.float32)
 
 denoise_idxs = [x ** 2 - 1 for x in range(1, 12)]
 
-for idx, fname in enumerate(imgs_list[1:]):
+for idx, fname in enumerate(jpgs_list[1:]):
     img = load_image(fname)
     raw = load_image(raw_list[idx])
-    # points = getStarCoords(img, num_stars=num_stars)
     transform = ecc_transform(init_img, img, prior=trans_prior)
-    # print(transform)
-    transforms.append(transform)
     trans_prior = transform
     img_stack_dim = img.shape
     frame_dim = (init_img.shape[1], init_img.shape[0])
     img_warp = cv2.warpPerspective(raw, transform, frame_dim,
-    borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 255, 0),
+    borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0),
     flags=cv2.WARP_INVERSE_MAP)
     img_stack += img_warp
     if idx in denoise_idxs:
@@ -46,11 +38,15 @@ for idx, fname in enumerate(imgs_list[1:]):
         w = 300
         img_out = img_stack[center[0] - h:center[0] + h,
                             center[1] - w:center[1] + w] / (idx + 1)
-        out = str(idx + 1) + '_frames_mean.jpg'
-        print(out) 
+        out = 'to_gif/frame_' + str(idx).zfill(3) + '.jpg'
+        print(out, "saved") 
         cv2.imwrite(out, img_out)
-    print(str(idx), fname, "stacked")
-img_stack /= len(imgs_list)
-out_name = "stack_mean.jpg"
-cv2.imwrite(out_name, img_stack)
+    print(fname, "stacked, frame", str(idx + 2), "of", str(len(jpgs_list)))
+
+img_stack /= len(jpgs_list)
+stars, background = rm_background(img_stack)
+stars = cv2.cvtColor(stars, cv2.COLOR_BGR2RGB)
+out_name = "stars.jpg"
+cv2.imwrite(out_name, stars)
+cv2.imwrite("removed_background.jpg", background)
 print(out_name, "saved")
