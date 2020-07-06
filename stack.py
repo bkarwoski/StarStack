ab4b4d2281a6a28826d8e42e42b4aec74eaa1180
Blake Karwoski <31117797+bkarwoski@users.noreply.github.com>
2020-07-05 20:14:25 -0700

    modify saving and frame debugging ability

--- stack.py
+++ stack.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
+from util import *
+
 import glob
 import os
 import pathlib
-
-from sklearn.neighbors import KDTree
 import sys
-from util import *
 
 jpgs_path = os.path.join(pathlib.Path().absolute(), "jpg/*")
 raw_path = os.path.join(pathlib.Path().absolute(), "raw/*")
@@ -17,6 +16,7 @@ init_img = load_image(jpgs_list[0])
 img_stack = init_img.astype(np.float32)
 
 denoise_idxs = [x ** 2 - 1 for x in range(1, 12)]
+denoise_idxs = [0, 1, 3, 7, 15, 31, 63, 119]
 
 for idx, fname in enumerate(jpgs_list[1:]):
     img = load_image(fname)
@@ -35,13 +35,15 @@ for idx, fname in enumerate(jpgs_list[1:]):
         w = 300
         img_out = img_stack[center[0] - h:center[0] + h,
                             center[1] - w:center[1] + w] / (idx + 1)
-        out = 'to_gif/frame_' + str(idx).zfill(3) + '.jpg'
+        out = 'to_gif/frame_' + str(idx + 1).zfill(3) + '.jpg'
         print(out, "saved") 
         cv2.imwrite(out, img_out)
     print(fname, "stacked, frame", str(idx + 2), "of", str(len(jpgs_list)))
 
 img_stack /= len(jpgs_list)
 stars, background = rm_background(img_stack)
+np.save("stars.npy", stars)
+np.save("background.npy", background)
 stars = cv2.cvtColor(stars, cv2.COLOR_BGR2RGB)
 background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
 out_name = "stars.jpg"
